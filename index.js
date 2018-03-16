75e3987017b3e12100e06466f740a92832c7b9d5
Tran Huu Khanh Van <kv090695@gmail.com>
2018-03-16 17:39:23 +0700

    update

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
+app.use('/public', express.static(__dirname + '/public'));
 app.listen(3000, () => console.log('Server started'))
 app.get('/', (req, res) => res.render('home'))
