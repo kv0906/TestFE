1dda1e0a34ca24b58063bf18ffde142dfc394b27
Tran Huu Khanh Van <kv090695@gmail.com>
2018-03-17 09:14:12 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 
 const app = express()
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080
+
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
-app.use('/public', express.static(__dirname + '/public'));
-app.listen(3000, () => console.log('Server started'))
+app.use('/public', express.static(__dirname + '/public'))
+app.listen(port, () => console.log('Server started'))
 app.get('/', (req, res) => res.render('home'))
