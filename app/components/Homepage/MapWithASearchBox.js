df160112a92e0180251b72fa6dfa0ead659b3ae7
Tran Huu Khanh Van <kv090695@gmail.com>
2018-03-17 10:02:23 +0700

    update

--- app/components/Homepage/MapWithASearchBox.js
+++ app/components/Homepage/MapWithASearchBox.js
@@ -12,7 +12,7 @@ const { InfoBox } = require('react-google-maps/lib/components/addons/InfoBox')
 
 const MapWithASearchBox = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAWvDeudJ4Rb6GIKNgY1raEH4beiT1WJaA&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
