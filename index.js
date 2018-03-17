63a1c6f54bb69d76d7d62cffe23f399e912dd7b4
Tran Huu Khanh Van <kv090695@gmail.com>
2018-03-17 09:36:30 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
-app.use(express.static(__dirname + '/public'))
-
+app.use('/public', express.static(__dirname + '/public'));
 app.listen(port, () => console.log('Server started'))
 app.get('/', (req, res) => res.render('home'))
